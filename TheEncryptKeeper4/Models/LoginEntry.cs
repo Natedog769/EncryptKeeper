412478146d2e354ee5a3dbe9eb05c28482e6d17d
Natedog769 <42854752+Natedog769@users.noreply.github.com>
2024-04-04 17:24:22 -0700

    Initial upload
    Added all the current project files to the repo excluding the packages

---
+++ TheEncryptKeeper4/Models/LoginEntry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheEncryptKeeper4.Models
+{
+    public class LoginEntry
+    {
+        public bool IsSelected { get; set; }
+        public int ID { get; set; }
+        public string Website { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Notes { get; set; }
+
+        public void ClearValues()
+        {
+            Website = string.Empty;
+            Username = string.Empty;
+            Password = string.Empty;
+            Email = string.Empty;
+            Notes = string.Empty;
+        }
+
+    }
+}
---
+++ TheEncryptKeeper4/Services/EncryptorService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+///     The role of this service will be to take in a string and encrypt it, or decrypt it with some sort of algorith I make up
+///     There can be different methods for ecrypting maybe
+/// </summary>
+namespace TheEncryptKeeper4.Services
+{
+    public class EncryptorService
+    {
+
+        private Dictionary<char, char> Cipher = new Dictionary<char, char>();
+
+        private IList<char> keys = new List<char>();
+        private IList<char> values = new List<char>();
+
+
+        public EncryptorService()
+        {
+            for (char index = 'A'; index < 'Z'; index++)
+            {
+                keys.Add(index);
+            }
+            for (char index = 'a'; index < 'a'; index++)
+            {
+                keys.Add(index);
+            }
+
+            //create the values
+        }
+
+
+        //so first we will create a simple encryption method
+        public string EncryptString(string toEncrypt)
+        {
+            string encryption = "";
+
+            foreach (char letter in toEncrypt)
+            {
+                if (letter == 'Z')
+                {
+                    encryption += ('A');
+                }
+                else if (letter == 'z')
+                {
+                    encryption += 'a';
+                }
+                else
+                {
+                    encryption += letter + 1;
+                }
+            }
+
+            return encryption;
+        }
+
+        public string DecryptString(string toDecrypt)
+        {
+            string decryption = "";
+
+            foreach (char letter in toDecrypt)
+            {
+
+            }
+
+            return decryption;
+        }
+
+    }
+}
